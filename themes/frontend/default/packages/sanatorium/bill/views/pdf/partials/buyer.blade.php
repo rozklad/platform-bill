6dccf897bb888aa4056760703f05f5b5c2577b0e
Macbook <admin@Matysek-MacBook-Pro.local>
2016-08-26 15:17:38 +0200

    automatic commit

--- themes/frontend/default/packages/sanatorium/bill/views/pdf/partials/buyer.blade.php
+++ themes/frontend/default/packages/sanatorium/bill/views/pdf/partials/buyer.blade.php
@@ -1,8 +1,6 @@
 <h2>Buyer</h2>
 <strong>{{ $buyer->name }}</strong><br>
-{{ $buyer->address_line_1 }}<br>
-{{ $buyer->address_line_2 }}<br>
-{{ $buyer->address_line_3 }}<br>
+{{ $buyer->client_address }}<br>
 <br>
 {{ $buyer->tax_id }}<br>
 {{ $buyer->vat_id }}<br>
--- themes/frontend/default/packages/sanatorium/bill/views/pdf/partials/supplier.blade.php
+++ themes/frontend/default/packages/sanatorium/bill/views/pdf/partials/supplier.blade.php
@@ -1,8 +1,6 @@
 <h2>Supplier</h2>
 <strong>{{ $supplier->name }}</strong><br>
-{{ $supplier->address_line_1 }}<br>
-{{ $supplier->address_line_2 }}<br>
-{{ $supplier->address_line_3 }}<br>
+{{ $supplier->client_address }}<br>
 <br>
 {{ $supplier->tax_id }}<br>
 {{ $supplier->vat_id }}<br>
